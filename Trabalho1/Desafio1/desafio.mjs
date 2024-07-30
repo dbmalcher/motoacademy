4c71f2ff053297c252d8c0e70e9b469f75f66b16
daniel <danielmalcherdm@gmail.com>
2024-07-29 20:25:49 -0400

    Continue Desafio1

--- Trabalho1/Desafio1/desafio.mjs
+++ Trabalho1/Desafio1/desafio.mjs
@@ -1,17 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
     const searchButton = document.getElementById('searchButton')
     const userInput = document.getElementById('userCep').value
+    const result = document.getElementById('result')
 
     searchButton.addEventListener('click',() => {
         event.preventDefault()
         
         //função de tratamento do cep vem aqui
+        checkCEP(userInput);
         
-        const url = "viacep.com.br/ws/" + userInput + "/json/"
+        const url = "https://viacep.com.br/ws/" + userInput + "/json/"
 
-        const requestCep = new XMLHttpRequest();
-        requestCep.open('GET', url ,true);
-        requestCep.send();
-        //return cep;
+        getData(url, result);
     })
 })
+
+async function getData(url, result) {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      console.log(json);
+
+      result.innerHTML = `
+            <h2>Resultado</h2>
+            <p>Bairro: ${json.bairro}</p>
+            <p>Complemento: ${json.complemento}</p>
+            <p>DDD: ${json.ddd}</p>
+            <p>Localidade: ${json.localidade}</p>
+            <p>Logradouro: ${json.logradouro}</p>
+        `
+      
+    } catch (error) {
+      console.error(error.message);
+    }
+}
+
+function checkCEP(cep){
+    if(cep.length != 11){
+        return false
+    }
+    return isNaN(cep)
+}
+  
