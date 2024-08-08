84d1143b1eebec49dba87839a03751031526357d
daniel <danielmalcherdm@gmail.com>
2024-08-07 20:23:38 -0400

    Add GET and POST requests in index.js

--- 5_NodeJS/index.js
+++ 5_NodeJS/index.js
@@ -1,6 +1,29 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json())
+
+const users =  [
+    {id: 1, name: "Euclides"},
+    {id: 2, name: "Filipe"}
+] 
+
+app.get('/users', (req,res) => {
+    res.json(users);
+})
+
+app.post('/users',(req,res) => {
+    const name = req.body.name
+    const id = users.length + 1
+
+    const newUser = {
+        id: id,
+        name: name,
+    }
+
+    users.push(newUser)
+    res.status(201).json(newUser)
+})
 
 app.get('/', (req,res) => {
     res.send({message: "Olá mundo"})
