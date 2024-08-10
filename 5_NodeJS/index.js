78ccf9a569e48d330dc05464d84ae29cdfb4ce0c
daniel <danielmalcherdm@gmail.com>
2024-08-09 20:15:58 -0400

    Add nodemon and put and delete methods

--- 5_NodeJS/index.js
+++ 5_NodeJS/index.js
@@ -3,13 +3,13 @@ const express = require('express');
 const app = express();
 app.use(express.json())
 
-const users =  [
+let users =  [
     {id: 1, name: "Euclides"},
     {id: 2, name: "Filipe"}
 ] 
 
 app.get('/users', (req,res) => {
-    res.json(users);
+    res.status(200).json(users);
 })
 
 app.post('/users',(req,res) => {
@@ -23,6 +23,25 @@ app.post('/users',(req,res) => {
 
     users.push(newUser)
     res.status(201).json(newUser)
+})
+
+app.put('/users/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    const { name } = req.body //pega o body que vai ser alterado
+    const userIndex = users.findIndex( user => user.id == id) //procura o index na array do objeto que tem o id == id
+
+    if(userIndex != -1){
+        users[userIndex].name = name
+        res.json(users[userIndex])
+    } else {
+        res.status(400).json({message: 'Usuário não encontrado'})
+    }
+})
+
+app.delete('/users/:id', (req,res) => {
+    const id = parseInt(req.params.id)
+    users = users.filter( user => user.id !== id)
+    res.sendStatus(204)
 })
 
 app.get('/', (req,res) => {
