6fd923a2b006cec8552e4f2d95ddee1a55d21e2d
daniel <danielmalcherdm@gmail.com>
2024-08-14 20:16:42 -0400

    Add middlewares to index.js

--- 5_NodeJS/index.js
+++ 5_NodeJS/index.js
@@ -1,31 +1,51 @@
 const express = require('express');
 
 const app = express();
+
 app.use(express.json())
 
+const loginAuthentication = (req, res, next) => {
+    const logado = req.headers.authorization == "Bearer tokenValido"
+    if(logado){
+        next()
+    } else {
+        res.status(401).json({message: "Por favor verifique suas credenciais"})
+    }
+}
+
 let users =  [
-    {id: 1, name: "Euclides"},
-    {id: 2, name: "Filipe"}
+    {id: 1, name: "Euclides", email:"eu@mail.com", senha:"12345"},
+    {id: 2, name: "Filipe", email:"vc@mail.com", senha:"12345"}
 ] 
 
-app.get('/users', (req,res) => {
+app.get('/users', loginAuthentication, (req,res) => {
     res.status(200).json(users);
 })
 
-app.post('/users',(req,res) => {
+app.post('/login', (req,res) => {
+    const { name, email, senha} = req.body
+    const user = users.find(user => user.email == email && user.senha == senha)
+    if(user){
+        res.json({message: "Login bem sucedido", token: "tokenValido"})
+    } else {
+        res.json({message: "Credenciais Inválidas"})
+    }
+})
+
+app.post('/users', loginAuthentication,(req,res) => {
     const name = req.body.name
     const id = users.length + 1
 
     const newUser = {
         id: id,
-        name: name,
+        name: name 
     }
 
     users.push(newUser)
     res.status(201).json(newUser)
 })
 
-app.put('/users/:id', (req,res) => {
+app.put('/users/:id', loginAuthentication, (req,res) => {
     const id = parseInt(req.params.id)
     const { name } = req.body //pega o body que vai ser alterado
     const userIndex = users.findIndex( user => user.id == id) //procura o index na array do objeto que tem o id == id
@@ -38,13 +58,13 @@ app.put('/users/:id', (req,res) => {
     }
 })
 
-app.delete('/users/:id', (req,res) => {
+app.delete('/users/:id', loginAuthentication, (req,res) => {
     const id = parseInt(req.params.id)
     users = users.filter( user => user.id !== id)
     res.sendStatus(204)
 })
 
-app.get('/', (req,res) => {
+app.get('/', loginAuthentication, (req,res) => {
     res.send({message: "Olá mundo"})
 })
 
