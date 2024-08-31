8ae512654328685c699ead5861f6e7f21d295eda
daniel <danielmalcherdm@gmail.com>
2024-08-30 20:11:17 -0400

    Add get images

--- 6_BancoDeDados/index.js
+++ 6_BancoDeDados/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes')
+const fileRoute = require ('./routes/fileRoute.js')
 
 app.use(express.json());
 app.use('/api', userRoutes)
+app.use('/api/files', fileRoute)
 
 app.listen(3000, () => {
     console.log("Servidor executando na porta 3000");
