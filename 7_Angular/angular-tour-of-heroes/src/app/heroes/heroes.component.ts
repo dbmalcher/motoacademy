3ca358f0b9c73193bdcad54e6d9b6c03b7e305f2
daniel <danielmalcherdm@gmail.com>
2024-09-23 20:15:19 -0400

    Create new angular project Tour of Heroes

---
+++ 7_Angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {Hero} from "../hero"
+import { CommonModule, NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {HEROES} from '../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [CommonModule, FormsModule, NgFor, HeroDetailComponent],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+
+export class HeroesComponent {
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
